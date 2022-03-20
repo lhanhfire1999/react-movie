f57bcec1ee3392ded4889e79e7ee27bee3235a69
Anh Le <lhanhfire1999@gmail.com>
2022-03-21 00:36:13 +0700

    CarouselNavigation component

--- src/components/MovieDetails/TrailerList.jsx
+++ src/components/MovieDetails/TrailerList.jsx
@@ -1,47 +1,87 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { getTrailerThumbUrl, getTrailerUrl } from '../../utils';
+import React, { useCallback, useLayoutEffect, useRef } from 'react';
+
 import { $ } from '../../constants';
+import { getTrailerThumbUrl, getTrailerUrl } from '../../utils';
+import CarouselNavigation from '../CarouselNavigation';
 
 const TrailerList = ({ videos }) => {
-  const handlePlayModal = (id, key) => {
+  const trailersRef = useRef(null);
+  const navigationRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const { clientWidth, scrollWidth } = trailersRef.current;
+    trailersRef.current.scrollLeft = 0;
+
+    if (scrollWidth === clientWidth) {
+      navigationRef.current.removeClassName(['next', 'previous']);
+    } else if (scrollWidth > clientWidth) {
+      navigationRef.current.addClassName('next');
+      navigationRef.current.removeClassName('previous');
+    }
+  }, [videos]);
+
+  const handleTurnOnTrailer = (id, key) => {
     $(`#trailer-modal-${id} iframe`).setAttribute('src', getTrailerUrl(key));
     $(`#trailer-modal-${id}`).classList.add('active');
   };
 
-  return (
-    <ul className="trailer-list">
-      {videos.map((video) => (
-        <li
-          key={video?.id}
-          className="trailer-card"
-          onClick={() => handlePlayModal(video?.id, video?.key)}
-        >
-          <div
-            className="trailer-card__img"
-            style={{
-              backgroundImage: `url(${
-                getTrailerThumbUrl(video?.key).standard
-              })  `,
-            }}
-          >
-            <span className="play-btn">
-              <i className="bx bx-play-circle bx-lg"></i>
-            </span>
-          </div>
-          <h3 className="trailer-card__title">{video?.name}</h3>
-        </li>
-      ))}
+  const handleCaroselNavigation = useCallback((direction) => {
+    let scrollLeft = trailersRef.current.scrollLeft;
+    const { clientWidth, scrollWidth } = trailersRef.current;
+    const { addClassName, removeClassName } = navigationRef.current;
+    const scrollMaxWidth = scrollWidth - clientWidth;
 
-      <div className="btns">
-        <div className="btn-next">
-          <i className="bx bx-chevron-right bx-md"></i>
-        </div>
-        <div className="btn-back">
-          <i className="bx bx-chevron-left bx-md"></i>
-        </div>
-      </div>
-    </ul>
+    if (direction === 'next') {
+      trailersRef.current.scrollLeft += 440;
+      scrollLeft += 440;
+    } else if (direction === 'prev') {
+      trailersRef.current.scrollLeft -= 440;
+      scrollLeft -= 440;
+    }
+
+    if (scrollLeft <= 0) {
+      removeClassName('previous');
+      addClassName('next');
+    } else if (scrollLeft > 0 && scrollLeft < scrollMaxWidth) {
+      addClassName(['next', 'previous']);
+    } else if (scrollLeft >= scrollMaxWidth) {
+      removeClassName('next');
+      addClassName('previous');
+    }
+  }, []);
+
+  return (
+    <div className="trailer-list">
+      <ul className="trailer-list__container" ref={trailersRef}>
+        {videos.map((video) => (
+          <li
+            key={video?.id}
+            className="trailer-card"
+            onClick={() => handleTurnOnTrailer(video?.id, video?.key)}
+          >
+            <div
+              className="trailer-card__img"
+              style={{
+                backgroundImage: `url(${
+                  getTrailerThumbUrl(video?.key).standard
+                })  `,
+              }}
+            >
+              <span className="play-btn">
+                <i className="bx bx-play-circle bx-lg"></i>
+              </span>
+            </div>
+            <h3 className="trailer-card__title">{video?.name}</h3>
+          </li>
+        ))}
+      </ul>
+
+      <CarouselNavigation
+        onClick={handleCaroselNavigation}
+        ref={navigationRef}
+      />
+    </div>
   );
 };
 
