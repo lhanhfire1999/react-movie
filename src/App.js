4cd1c9e89b2f672d9f62aba6f5d3260772add9ec
Anh Le <lhanhfire1999@gmail.com>
2022-04-11 23:07:57 +0700

    lazy-loading for React-router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import 'boxicons/css/boxicons.min.css';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { NotFound } from './components';
-import {
-  Category,
-  Detail,
-  Filter,
-  Home,
-  MainLayout,
-  Search,
-  WatchMovie,
-} from './views';
+import { NotFound, Preloader } from './components';
 import './scss/App.scss';
+
+const Category = lazy(() => import('./views/Category'));
+const Detail = lazy(() => import('./views/Detail'));
+const Filter = lazy(() => import('./views/Filter'));
+const Home = lazy(() => import('./views/Home'));
+const MainLayout = lazy(() => import('./views/MainLayout'));
+const Search = lazy(() => import('./views/Search'));
+const WatchMovie = lazy(() => import('./views/WatchMovie'));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path=":category" element={<Category />} />
-          <Route path=":category/:movieId" element={<Detail />} />
-          <Route path="/watch/:category/:movieId" element={<WatchMovie />} />
-          <Route path="search" element={<Search />} />
-          <Route path="filter" element={<Filter />} />
+      <Suspense fallback={<Preloader />}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path=":category" element={<Category />} />
+            <Route path=":category/:movieId" element={<Detail />} />
+            <Route path="/watch/:category/:movieId" element={<WatchMovie />} />
+            <Route path="search" element={<Search />} />
+            <Route path="filter" element={<Filter />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
